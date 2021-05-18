9a8e8355482b0f83e441257f3db681a9409c14ed
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-18 16:28:33 +0200

    Update Set_Time.py

--- rtc/Set_Time.py
+++ rtc/Set_Time.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3.9
 #coding: utf-8
 
-
 from smbus2 import *
 import time
 
@@ -13,30 +12,27 @@ bus  = SMBus(1)
 SECONDS   = 0x03
 MINUTES   = 0x04
 HOURS     = 0x05
-
 DAYS      = 0x06
 WEEKDAYS  = 0x07
 MONTHS    = 0x08
 YEARS     = 0x09
-
 CONTROL_1 = 0x00
 
 def Set_time():
-    heures   = hex(input("Heures : "))
-    minutes  = int(input("Minutes : "))
-    secondes = int(input("Secondes : "))
+    heures   = input("Heures : ")
+    minutes  = input("Minutes : ")
+    secondes = input("Secondes : ")
     return [heures,minutes,secondes]
 
 
 def Set_time_RTC():
     
     heures,minutes,secondes = Set_time()
-
     print("L'heure va être synchronisé sur : {0} Heures, {1} Minutes, {2} Secondes".format(heures,minutes,secondes))
-
+    heures=int(heures)
+    minutes=int(minutes)
+    secondes=int(secondes)
     bus.write_i2c_block_data(SLAVE_ADDRESS, SECONDS, [heures,minutes,secondes])
-
-
 
 Set_time_RTC()
 
