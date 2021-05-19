94446a564f91fd399bbe95af73d898bc999cc060
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-19 09:24:16 +0200

    Update ALARM_return_zero.py

--- rtc/ALARM_return_zero.py
+++ rtc/ALARM_return_zero.py
@@ -39,13 +39,13 @@ data = [0x00, 0x02, 0xE0, 0x00, 0x00, 0x00]
 ######## R1 ,  R2 ,  R3 , sec ,  h  , min ####
 
 bus.write_i2c_block_data(SLAVE_ADDRESS, CONTROL_1, data)
-bus.write_i2c_block_data(SLAVE_ADDRESS, SECOND_ALARM, [0x02])
+bus.write_i2c_block_data(SLAVE_ADDRESS, SECOND_ALARM, [0x01])
 bus.write_i2c_block_data(SLAVE_ADDRESS, MINUTE_ALARM, [0x00])
 bus.write_i2c_block_data(SLAVE_ADDRESS, HOUR_ALARM, [0x00])
 bus.write_i2c_block_data(SLAVE_ADDRESS, DAY_ALARM, [0x80])
 bus.write_i2c_block_data(SLAVE_ADDRESS, WEEKDAY_ALARM, [0x80])
 
-time.sleep(4)
+time.sleep(2)
 bus.write_i2c_block_data(SLAVE_ADDRESS, CONTROL_2, [0x00])
 
 
