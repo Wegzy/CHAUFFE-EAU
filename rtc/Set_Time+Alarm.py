aab9ee36c99648d7a181e218ad694d260ffd0028
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-19 11:00:52 +0200

    Update Set_Time+Alarm.py

--- rtc/Set_Time+Alarm.py
+++ rtc/Set_Time+Alarm.py
@@ -44,7 +44,7 @@ def Set_time_RTC():
     rtc_min = hex(val[3])
     rtc_hours = hex(val[4])
     print("Il est {0} heures, {1} minutes et {2} secondes".format(rtc_sec[2:4],rtc_min[2:4],rtc_hours[2:4]))
-    print(rtc_sec+1)
+    print((int(rtc_sec+1))
 
 
 
