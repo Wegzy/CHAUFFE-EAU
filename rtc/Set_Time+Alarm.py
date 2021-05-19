a5824678b7689de0f9e4ae6d69ab9ddc4af00503
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-19 11:51:56 +0200

    Update Set_Time+Alarm.py

--- rtc/Set_Time+Alarm.py
+++ rtc/Set_Time+Alarm.py
@@ -42,7 +42,7 @@ def Set_time_RTC():
     print("L'heure va être synchronisé sur : {0} Heures, {1} Minutes, {2} Secondes".format(heures,minutes,secondes))
     
     secondes_alarm = int(secondes)+1
-    secondes_alarm = int(secondes_alarm ,16)
+    #secondes_alarm = int(secondes_alarm ,16)
     heures = int(hex_form+str(heures),16)
     minutes= int(hex_form+str(minutes),16) 
     secondes= int(hex_form+str(secondes),16)
