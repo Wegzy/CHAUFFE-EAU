481459147af3933e7b91ce5b1818b1589ffcab6d
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-20 15:23:11 +0200

    Update Set_Time+Alarm.py

--- rtc/Set_Time+Alarm.py
+++ rtc/Set_Time+Alarm.py
@@ -40,8 +40,11 @@ def Set_time_RTC():
     
       
     secondes = hex(int(secondes,16))
+    secondes = int(secondes)
     minutes  = hex(int(minutes ,16))
+    minutes = int(minutes)
     heures   = hex(int(heures  ,16))
+    heures = int(heures)
     
 
     print("L'heure va être reellement synchronisée sur : {0} Heures, {1} Minutes, {2} Secondes".format(heures,minutes,secondes))
@@ -49,12 +52,14 @@ def Set_time_RTC():
 
     bus.write_i2c_block_data(SLAVE_ADDRESS, CONTROL_2, [0x02])
     bus.write_i2c_block_data(SLAVE_ADDRESS, SECONDS, [secondes,minutes,heures])
+
     val = bus.read_i2c_block_data(SLAVE_ADDRESS, CONTROL_1, 9)
     print(val[2:5]) # Récupère uniquement les secondes, min, sec
     rtc_sec = hex(val[2]) # Passage en hexa de la valeur
     rtc_min = hex(val[3])
     rtc_hours = hex(val[4])
     print("Il est {0} heures, {1} minutes et {2} secondes".format(rtc_hours[2:4],rtc_min[2:4],rtc_sec[2:4]))
+
     print(secondes_alarm) # Vérifier l'incrémentation de l'heure
     bus.write_i2c_block_data(SLAVE_ADDRESS, SECOND_ALARM, [secondes_alarm, minutes, heures, 0x80, 0x80])
     
