419067598d14b33499c130b00845eae6322047bd
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-05-20 15:13:51 +0200

    Update Set_Time+Alarm.py

--- rtc/Set_Time+Alarm.py
+++ rtc/Set_Time+Alarm.py
@@ -34,12 +34,10 @@ def Set_time_RTC():
 
     print("L'heure va être synchronisée sur : {0} Heures, {1} Minutes, {2} Secondes".format(heures,minutes,secondes))
         
-    secondes_alarm = int(secondes)
-    print("Secondes alarme : {0}".format(secondes_alarm))
-    secondes_alarm = secondes_alarm+2
-    print("Secondes alarme : {0}\n\n".format(secondes_alarm))
+    secondes_alarm = int(secondes)+2
     secondes_alarm = str(secondes_alarm)
-    print("Secondes alarme : {0}\n\n".format(secondes_alarm))
+    secondes_alarm = hex(int(secondes_alarm,16))
+    
       
     secondes = hex(int(secondes,16))
     minutes  = hex(int(minutes ,16))
@@ -49,6 +47,20 @@ def Set_time_RTC():
     print("L'heure va être reellement synchronisée sur : {0} Heures, {1} Minutes, {2} Secondes".format(heures,minutes,secondes))
 
 
+    bus.write_i2c_block_data(SLAVE_ADDRESS, CONTROL_2, [0x02])
+    bus.write_i2c_block_data(SLAVE_ADDRESS, SECONDS, [secondes,minutes,heures])
+    val = bus.read_i2c_block_data(SLAVE_ADDRESS, CONTROL_1, 9)
+    print(val[2:5]) # Récupère uniquement les secondes, min, sec
+    rtc_sec = hex(val[2]) # Passage en hexa de la valeur
+    rtc_min = hex(val[3])
+    rtc_hours = hex(val[4])
+    print("Il est {0} heures, {1} minutes et {2} secondes".format(rtc_hours[2:4],rtc_min[2:4],rtc_sec[2:4]))
+    print(secondes_alarm) # Vérifier l'incrémentation de l'heure
+    bus.write_i2c_block_data(SLAVE_ADDRESS, SECOND_ALARM, [secondes_alarm, minutes, heures, 0x80, 0x80])
     
+    time.sleep(4)
+    bus.write_i2c_block_data(SLAVE_ADDRESS, CONTROL_2, [0x00])
+
+
 Set_time_RTC()
 
