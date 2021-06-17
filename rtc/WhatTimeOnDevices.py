e16d49df5d62eec46d5bc93db58d8e7823d37b8d
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-06-17 16:38:53 +0200

    Update WhatTimeOnDevices.py

--- rtc/WhatTimeOnDevices.py
+++ rtc/WhatTimeOnDevices.py
@@ -4,27 +4,23 @@ import time
 
 def timenow_rpi():    
     try:
-        global Heure_Recuperee_RPI
 
         out = str(check_output("date +%X", shell=True), 'UTF-8')
         heures_rpi = out[0:2]
         minutes_rpi = out[3:5]
         secondes_rpi = out[6:8]
         print("\n ----> Il est {0} heures, {1} minutes et {2} secondes sur la Raspberry".format(heures_rpi,minutes_rpi,secondes_rpi))
-        Heure_Recuperee_RPI = 0
-        return heures_rpi, minutes_rpi, secondes_rpi, Heure_Recuperee_RPI
+        
+        return heures_rpi, minutes_rpi, secondes_rpi
         
     except: 
         print(" Impossible de récupérer l'heure de la Raspberry ! ")
-        Heure_Recuperee_RPI = 1
-        return Heure_Recuperee_RPI
-
+        
 def timenow_RTC():
     SLAVE_ADDRESS = 0x51
     bus  = SMBus(1)
     CONTROL_1 = 0x00
-    try:
-        global Heure_Recuperee_RTC
+    try:        
         TimeOnRTC = bus.read_i2c_block_data(SLAVE_ADDRESS, CONTROL_1, 9)
         #print(val[2:5]) # Récupère uniquement les secondes, min, sec
         #print(type(TimeOnRTC))
@@ -39,23 +35,21 @@ def timenow_RTC():
         heures_RTC   = int(heures_RTC)    
 
         print("\n ----> Il est {0} heures, {1} minutes et {2} secondes sur la RTC \n".format(heures_RTC,minutes_RTC,secondes_RTC))
-        Heure_Recuperee_RTC = 0
-        return secondes_RTC,minutes_RTC,heures_RTC,Heure_Recuperee_RTC
+        
+        return secondes_RTC,minutes_RTC,heures_RTC
         
     except: 
         print("\n ----> Impossible de communiquer avec la RTC ! \n Vérifier son branchement !  \n ")
-        Heure_Recuperee_RTC = 1 
-        return Heure_Recuperee_RTC
-
+        
 try: 
     secondes_rpi, minutes_rpi, heures_rpi = timenow_rpi()
     secondes_RTC, minutes_RTC, heures_RTC = timenow_RTC()
 
-    if secondes_rpi == secondes_RTC and minutes_rpi == minutes_RTC: 
+    if secondes_rpi==secondes_RTC and minutes_rpi==minutes_RTC: 
         print("L'horloge RTC et la Raspberry sont synchronisés ! ")
     else:
         print("Les deux horloges ne sont pas synchronisés ! ")
 
 except:
 
-    print("L'heure n'a pas pu être comparé, ")
+    print("L'heure n'a pas pu être comparé ")
