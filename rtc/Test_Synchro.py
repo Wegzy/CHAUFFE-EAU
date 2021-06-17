22f30eca0df1e843d1c85bb710a2abe3f5d9ca1c
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-06-17 12:15:09 +0200

    Test_Sync
    Fix : Test sync patché dans Test_Synchro devenant prochainement sync.py.

--- rtc/Test_Synchro.py
+++ rtc/Test_Synchro.py
@@ -8,16 +8,22 @@ def check_sync():
     out = str(check_output("timedatectl | grep clock", shell=True), 'UTF-8')
     out = out[27:30]
     
+    statut = None
+    print(statut)
+
     if out == "yes":
         ip = "1.debian.pool.ntp.org"
         try:
             check_output("ping -c 2 " + ip, shell=True)
-            print("\n\n Statut 1 : L'horloge est synchronisé \n\n\n")
-        
+            print("\n\n Statut 0 : L'horloge est synchronisé \n\n\n")
+            statut = 0
+            print(statut)
         except CalledProcessError: 
-            print("\n\n Statut 2 : L'horloge n'est plus synchronisé à internet \n\n\n")
-    
+            print("\n\n Statut 1 : L'horloge n'est plus synchronisé à internet \n\n\n")
+            statut = 1
+            print(statut)
     else:
-        print("\n\n Statut 3 : L'horloge n'est plus du tout synchronisé \n\n\n")
-        
+        print("\n\n Statut 2 : L'horloge n'est plus du tout synchronisé \n\n\n")
+        statut = 2
+        print(statut)
 check_sync()    
