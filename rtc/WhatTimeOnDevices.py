9473f44063d0b4dd5d8f7073f070bff431db4127
FeZzko <40919725+FeZzko@users.noreply.github.com>
2021-06-18 07:28:52 +0200

    Update WhatTimeOnDevices.py

--- rtc/WhatTimeOnDevices.py
+++ rtc/WhatTimeOnDevices.py
@@ -73,7 +73,7 @@ try:
         print("L'horloge RTC et la Raspberry sont synchronisés ! ")
     else:
         print("L'horloge RTC et la Raspberry ne sont pas synchronisés ! ")
-        if int(heures_rpi) > int(heures_RTC):
+        if statut == 0 and int(heures_rpi) > int(heures_RTC):
             print("La raspberry est en avance sur la RTC")
 
 
